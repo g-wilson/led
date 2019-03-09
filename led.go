e6403fd558ee7e8922b9bb5c4c6f9a7ed0e2e7a8
George Wilson <george@g-wilson.co.uk>
2019-03-09 18:39:37 +0000

    show f1 venue

--- led.go
+++ led.go
@@ -11,6 +11,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/g-wilson/led/f1"
 	"github.com/g-wilson/led/weather"
 
 	"github.com/joho/godotenv"
@@ -88,8 +89,15 @@ func drawFrame(c *image.RGBA) (*image.RGBA, error) {
 
 	addText(c, 0, -1, time.Now().UTC().Format("15:04"), &color.RGBA{255, 255, 255, 255})
 
-	addText(c, 0, 26, fmt.Sprintf("%02.f", weatherCache.Today.ApparentTemperatureLow)+"oC", &color.RGBA{80, 80, 255, 255})
-	addText(c, 17, 26, fmt.Sprintf("%02.f", weatherCache.Today.ApparentTemperatureHigh)+"oC", &color.RGBA{255, 150, 0, 255})
+	weatherX := 0
+	weatherY := 12
+	addText(c, weatherX, weatherY, fmt.Sprintf("%02.f", weatherCache.Today.ApparentTemperatureLow)+"oC", &color.RGBA{80, 80, 255, 255})
+	addText(c, weatherX+17, weatherY, fmt.Sprintf("%02.f", weatherCache.Today.ApparentTemperatureHigh)+"oC", &color.RGBA{255, 150, 0, 255})
+
+	race := f1.GetNextRace()
+	if race != nil {
+		addText(c, 0, 26, race.Location+": "+formatDuration(race.Until()), &color.RGBA{255, 20, 20, 255})
+	}
 
 	return c, nil
 }
@@ -102,4 +110,19 @@ func addText(c *image.RGBA, x, y int, text string, col *color.RGBA) {
 	fontFace.Scale(1)
 	fontFace.Color(col)
 	fontFace.DrawText(c, image.Point{X: x, Y: y}, text)
+}
+
+// Not actually days - 24h periods because that's much easier and honestly who needs daylight savings
+func formatDuration(u time.Duration) string {
+	u = u.Round(time.Minute)
+
+	d := u / (time.Hour * 24)
+	u -= d * (time.Hour * 24)
+
+	h := u / time.Hour
+	u -= h * time.Hour
+
+	m := u / time.Minute
+
+	return fmt.Sprintf("%02dd %02dh %02dm", d, h, m)
 }
