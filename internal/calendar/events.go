6c52e26d5e9c5cc70eba2f4bd61d539ad4b28004
George Wilson <george@g-wilson.co.uk>
2025-01-11 15:40:50 +0000

    add 2025 f1 calendar

--- internal/calendar/events.go
+++ internal/calendar/events.go
@@ -1,20 +1,31 @@
 package calendar
 
 var events = eventList{
-	// {
-	// 	Name:      "BAH",
-	// 	Timestamp: "2024-03-02T15:00:00.000Z",
-	// 	Image:     f1Image,
-	// },
+	{"AUS", "2025-03-16T00:00:00Z", f1Image}, // Australian Grand Prix
+	{"CHN", "2025-03-23T00:00:00Z", f1Image}, // Chinese Grand Prix
+	{"JPN", "2025-04-06T00:00:00Z", f1Image}, // Japanese Grand Prix
+	{"BHR", "2025-04-13T00:00:00Z", f1Image}, // Bahrain Grand Prix
+	{"SAU", "2025-04-20T00:00:00Z", f1Image}, // Saudi Arabian Grand Prix
+	{"MIA", "2025-05-04T00:00:00Z", f1Image}, // Miami Grand Prix
+	{"EMI", "2025-05-18T00:00:00Z", f1Image}, // Emilia Romagna Grand Prix
+	{"MCO", "2025-05-25T00:00:00Z", f1Image}, // Monaco Grand Prix
+	{"ESP", "2025-06-01T00:00:00Z", f1Image}, // Spanish Grand Prix
+	{"CAN", "2025-06-15T00:00:00Z", f1Image}, // Canadian Grand Prix
+	{"AUT", "2025-06-29T00:00:00Z", f1Image}, // Austrian Grand Prix
+	{"GBR", "2025-07-06T00:00:00Z", f1Image}, // British Grand Prix
+	{"BEL", "2025-07-27T00:00:00Z", f1Image}, // Belgian Grand Prix
+	{"HUN", "2025-08-03T00:00:00Z", f1Image}, // Hungarian Grand Prix
+	{"NED", "2025-08-31T00:00:00Z", f1Image}, // Dutch Grand Prix
+	{"ITA", "2025-09-07T00:00:00Z", f1Image}, // Italian Grand Prix
+	{"AZE", "2025-09-21T00:00:00Z", f1Image}, // Azerbaijan Grand Prix
+	{"SGP", "2025-10-05T00:00:00Z", f1Image}, // Singapore Grand Prix
+	{"USA", "2025-10-19T00:00:00Z", f1Image}, // United States Grand Prix
+	{"MEX", "2025-10-26T00:00:00Z", f1Image}, // Mexican Grand Prix
+	{"BRA", "2025-11-09T00:00:00Z", f1Image}, // Brazilian Grand Prix
+	{"LAS", "2025-11-22T00:00:00Z", f1Image}, // Las Vegas Grand Prix
+	{"QAT", "2025-11-30T00:00:00Z", f1Image}, // Qatar Grand Prix
+	{"UAE", "2025-12-07T00:00:00Z", f1Image}, // Abu Dhabi Grand Prix
 
-	{
-		Name:      "XMAS",
-		Timestamp: "2025-12-25T00:00:00.000Z",
-		Image:     xmasImage,
-	},
-	{
-		Name:      "2026",
-		Timestamp: "2026-01-0100:00:00.000Z",
-		Image:     xmasImage,
-	},
+	{"XMAS", "2025-12-25T00:00:00.000Z", xmasImage},
+	{"2026", "2026-01-0100:00:00.000Z", xmasImage},
 }
