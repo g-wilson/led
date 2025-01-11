d3333436b6fb11d4105616cd9f12e0b78f81457f
George Wilson <george@g-wilson.co.uk>
2025-01-11 15:32:32 +0000

    remove 2024 events

--- internal/calendar/events.go
+++ internal/calendar/events.go
@@ -1,134 +1,20 @@
 package calendar
 
 var events = eventList{
-	{
-		Name:      "BAH",
-		Timestamp: "2024-03-02T15:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "SAU",
-		Timestamp: "2024-03-09T17:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "AUS",
-		Timestamp: "2024-03-24T05:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "JAP",
-		Timestamp: "2024-04-07T05:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "CHN",
-		Timestamp: "2024-04-21T05:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "MIA",
-		Timestamp: "2024-05-05T19:30:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "EMI",
-		Timestamp: "2024-05-19T13:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "MON",
-		Timestamp: "2024-05-26T13:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "CAN",
-		Timestamp: "2024-06-09T18:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "ESP",
-		Timestamp: "2024-06-23T13:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "AUT",
-		Timestamp: "2024-06-30T13:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "GBR",
-		Timestamp: "2024-07-07T14:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "HUN",
-		Timestamp: "2024-07-21T13:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "BEL",
-		Timestamp: "2024-07-28T13:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "NED",
-		Timestamp: "2024-08-25T13:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "ITA",
-		Timestamp: "2024-09-01T13:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "AZE",
-		Timestamp: "2024-09-15T13:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "SIN",
-		Timestamp: "2024-09-22T12:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "USA",
-		Timestamp: "2024-10-20T19:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "MEX",
-		Timestamp: "2024-10-27T20:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "BRA",
-		Timestamp: "2024-11-03T17:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "LSV",
-		Timestamp: "2024-11-23T04:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "QAT",
-		Timestamp: "2024-12-01T14:00:00.000Z",
-		Image:     f1Image,
-	},
-	{
-		Name:      "ABU",
-		Timestamp: "2024-12-08T13:00:00.000Z",
-		Image:     f1Image,
-	},
+	// {
+	// 	Name:      "BAH",
+	// 	Timestamp: "2024-03-02T15:00:00.000Z",
+	// 	Image:     f1Image,
+	// },
+
 	{
 		Name:      "XMAS",
-		Timestamp: "2024-12-25T00:00:00.000Z",
+		Timestamp: "2025-12-25T00:00:00.000Z",
 		Image:     xmasImage,
 	},
 	{
-		Name:      "2025",
-		Timestamp: "2025-01-0100:00:00.000Z",
+		Name:      "2026",
+		Timestamp: "2026-01-0100:00:00.000Z",
 		Image:     xmasImage,
 	},
 }
