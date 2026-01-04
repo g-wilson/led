11ff48f610e8a98125ec869acb7799d80ab3b0c7
George Wilson <george.wilson@paddle.com>
2026-01-04 19:48:38 +0000

    add event calendar for 2026

--- internal/calendar/events.go
+++ internal/calendar/events.go
@@ -1,31 +1,33 @@
 package calendar
 
 var events = eventList{
-	{"AUS", "2025-03-16T00:00:00Z", f1Image}, // Australian Grand Prix
-	{"CHN", "2025-03-23T00:00:00Z", f1Image}, // Chinese Grand Prix
-	{"JPN", "2025-04-06T00:00:00Z", f1Image}, // Japanese Grand Prix
-	{"BHR", "2025-04-13T00:00:00Z", f1Image}, // Bahrain Grand Prix
-	{"SAU", "2025-04-20T00:00:00Z", f1Image}, // Saudi Arabian Grand Prix
-	{"MIA", "2025-05-04T00:00:00Z", f1Image}, // Miami Grand Prix
-	{"EMI", "2025-05-18T00:00:00Z", f1Image}, // Emilia Romagna Grand Prix
-	{"MCO", "2025-05-25T00:00:00Z", f1Image}, // Monaco Grand Prix
-	{"ESP", "2025-06-01T00:00:00Z", f1Image}, // Spanish Grand Prix
-	{"CAN", "2025-06-15T00:00:00Z", f1Image}, // Canadian Grand Prix
-	{"AUT", "2025-06-29T00:00:00Z", f1Image}, // Austrian Grand Prix
-	{"GBR", "2025-07-06T00:00:00Z", f1Image}, // British Grand Prix
-	{"BEL", "2025-07-27T00:00:00Z", f1Image}, // Belgian Grand Prix
-	{"HUN", "2025-08-03T00:00:00Z", f1Image}, // Hungarian Grand Prix
-	{"NED", "2025-08-31T00:00:00Z", f1Image}, // Dutch Grand Prix
-	{"ITA", "2025-09-07T00:00:00Z", f1Image}, // Italian Grand Prix
-	{"AZE", "2025-09-21T00:00:00Z", f1Image}, // Azerbaijan Grand Prix
-	{"SGP", "2025-10-05T00:00:00Z", f1Image}, // Singapore Grand Prix
-	{"USA", "2025-10-19T00:00:00Z", f1Image}, // United States Grand Prix
-	{"MEX", "2025-10-26T00:00:00Z", f1Image}, // Mexican Grand Prix
-	{"BRA", "2025-11-09T00:00:00Z", f1Image}, // Brazilian Grand Prix
-	{"LAS", "2025-11-22T00:00:00Z", f1Image}, // Las Vegas Grand Prix
-	{"QAT", "2025-11-30T00:00:00Z", f1Image}, // Qatar Grand Prix
-	{"UAE", "2025-12-07T00:00:00Z", f1Image}, // Abu Dhabi Grand Prix
+	{"AUS", "2026-03-08T00:00:00Z", f1Image}, // Australian Grand Prix
+	{"CHN", "2026-03-15T00:00:00Z", f1Image}, // Chinese Grand Prix
+	{"JPN", "2026-03-29T00:00:00Z", f1Image}, // Japanese Grand Prix
+	{"BHR", "2026-04-12T00:00:00Z", f1Image}, // Bahrain Grand Prix
+	{"SAU", "2026-04-19T00:00:00Z", f1Image}, // Saudi Arabian Grand Prix
+	{"MIA", "2026-05-03T00:00:00Z", f1Image}, // Miami Grand Prix
+	{"CAN", "2026-05-24T00:00:00Z", f1Image}, // Canadian Grand Prix
+	{"MON", "2026-06-07T00:00:00Z", f1Image}, // Monaco Grand Prix
+	{"CAT", "2026-06-14T00:00:00Z", f1Image}, // Barcelona-Catalunya Grand Prix
+	{"AUT", "2026-06-28T00:00:00Z", f1Image}, // Austrian Grand Prix
+	{"GBR", "2026-07-05T00:00:00Z", f1Image}, // British Grand Prix
+	{"BEL", "2026-07-19T00:00:00Z", f1Image}, // Belgian Grand Prix
+	{"HUN", "2026-07-26T00:00:00Z", f1Image}, // Hungarian Grand Prix
+	{"NED", "2026-08-23T00:00:00Z", f1Image}, // Dutch Grand Prix
+	{"ITA", "2026-09-06T00:00:00Z", f1Image}, // Italian Grand Prix
+	{"ESP", "2026-09-13T00:00:00Z", f1Image}, // Spanish Grand Prix
+	{"AZE", "2026-09-27T00:00:00Z", f1Image}, // Azerbaijan Grand Prix
+	{"SGP", "2026-10-11T00:00:00Z", f1Image}, // Singapore Grand Prix
+	{"USA", "2026-10-25T00:00:00Z", f1Image}, // United States Grand Prix
+	{"MEX", "2026-11-01T00:00:00Z", f1Image}, // Mexican Grand Prix
+	{"BRA", "2026-11-08T00:00:00Z", f1Image}, // Brazilian Grand Prix
+	{"LAS", "2026-11-21T00:00:00Z", f1Image}, // Las Vegas Grand Prix
+	{"QAT", "2026-11-29T00:00:00Z", f1Image}, // Qatar Grand Prix
+	{"UAE", "2026-12-06T00:00:00Z", f1Image}, // Abu Dhabi Grand Prix
 
-	{"XMAS", "2025-12-25T00:00:00.000Z", xmasImage},
-	{"2026", "2026-01-01T00:00:00.000Z", xmasImage},
+	{"Eurovision", "2026-05-16T19:00:00Z", nil}, // Eurovision Song Contest
+
+	{"XMAS", "2026-12-25T00:00:00.000Z", xmasImage},
+	{"2027", "2027-01-01T00:00:00.000Z", xmasImage},
 }
